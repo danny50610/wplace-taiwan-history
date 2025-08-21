d45c357343010337756474e933f83a0c3d6432fe
Chen, Danny <3949245+danny50610@users.noreply.github.com>
2025-08-21 11:45:52 +0800

    feat: Add 2025-08-21

--- src/data.js
+++ src/data.js
@@ -1,9 +1,10 @@
 const dataAt = {
     '2025-08-19': '1755532802',
     '2025-08-20': '1755619201',
-}
+    '2025-08-21': '1755705601',
+};
 
-export const lastDateString = '2025-08-20';
+export const lastDateString = '2025-08-21';
 export const lastDate = dataAt[lastDateString];
 
 export function hasData(date) {
